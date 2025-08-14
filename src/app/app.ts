9744b3c030d422c6d09a00dd098c04fe739afa21
Kamiltlusty <k.tlusty@student.uksw.edu.pl>
2025-08-14 15:32:33 +0200

    added new components [about us, portfolio, contact, footer]

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './header-component/header-component';
 import {CarouselComponent} from './carousel-component/carousel-component';
 import {AboutMeComponent} from './about-me-component/about-me-component';
+import {PortfolioComponent} from './portfolio-component/portfolio-component';
+import {ContactComponent} from './contact-component/contact-component';
+import {FooterComponent} from './footer-component/footer-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CarouselComponent, AboutMeComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    CarouselComponent,
+    AboutMeComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent
+  ],
   templateUrl: './app.html',
   standalone: true,
   styleUrl: './app.css'
---
+++ src/app/portfolio-component/portfolio-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-component',
+  imports: [],
+  templateUrl: './portfolio-component.html',
+  standalone: true,
+  styleUrl: './portfolio-component.css'
+})
+export class PortfolioComponent {
+
+}
