a1528373249e04fcf57a7fdd2487b18142a0dbce
Kamiltlusty <k.tlusty@student.uksw.edu.pl>
2025-08-29 00:56:54 +0200

    added initial template of a logback.xml

--- art-site-and-store-api/src/main/java/pl/kamil/artsiteandstoreapi/application/HomePageController.java
+++ art-site-and-store-api/src/main/java/pl/kamil/artsiteandstoreapi/application/HomePageController.java
@@ -1,6 +1,9 @@
 package pl.kamil.artsiteandstoreapi.application;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,12 +17,14 @@ import java.util.List;
 import java.util.UUID;
 
 
+@Slf4j
 @CrossOrigin("http://localhost:4200")
 @RestController
 @RequiredArgsConstructor
 public class HomePageController {
   private final ImageService imageService;
   private final PlaceMapper placeMapper;
+  private static final Logger logger = LoggerFactory.getLogger(HomePageController.class);
 
   @GetMapping("/carousel")
   public ResponseEntity<List<PlaceDTO>> getCarousel() {
