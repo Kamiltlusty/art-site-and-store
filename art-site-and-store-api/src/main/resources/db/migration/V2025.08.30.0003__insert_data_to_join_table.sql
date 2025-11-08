91a03e71e62a74f7c9b340580f30c2ce2a807ca7
Kamiltlusty <k.tlusty@student.uksw.edu.pl>
2025-11-08 14:16:55 +0100

    added activity diagram: Load main page

--- art-site-and-store-api/src/main/resources/db/migration/V2025.08.30.0003__insert_data_to_join_table.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO place_image (place_image_id, image_id, place_id)
-VALUES ('245f280f-276c-4627-ab56-f53d44a7f344', '723ff80b-d05d-4e0c-80f1-d0effabaf7d6', '7d6cc6c6-dbf2-4282-9b9e-cab8abcffd6b'),
-        ('e5b03bf0-2a9f-485d-92fb-9296dc723873', '575783ab-ba1c-454d-b6ef-557f4ed564ea', '7d6cc6c6-dbf2-4282-9b9e-cab8abcffd6b'),
-        ('c79febb5-f139-4cba-ac42-ce2f4cb5b80c', '909840d0-83af-4546-a3c3-ed33a44423a3', '7d6cc6c6-dbf2-4282-9b9e-cab8abcffd6b'),
-        ('738976d5-9508-4b7f-8b74-6d6bf180f642', 'dd4062cf-7f33-48f9-86ec-302edca5c3ab', '7d6cc6c6-dbf2-4282-9b9e-cab8abcffd6b'),
-        ('66833470-228a-4ca8-82ef-726088f62655','36de7f22-694f-4057-9377-cec58b175d9f', '7d6cc6c6-dbf2-4282-9b9e-cab8abcffd6b'),
-        ('ceb64883-961a-41c1-b65e-3898681a2992','687ac954-61e7-44fb-a730-4d8ed329490e', '7d6cc6c6-dbf2-4282-9b9e-cab8abcffd6b')
